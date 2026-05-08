e8b85a645972f1ce2d0aff6a05cca094bad677f1
nicholasgswan <nicholasgswan@gmail.com>
2026-05-08 13:45:46 -0700

    codecrafters submit [skip ci]

--- internal/path.go
+++ internal/path.go
@@ -20,11 +20,11 @@ func (p PathArray) CheckIfCommandExists(comm string) (bool, string) {
 	for _, dir := range p {
 		de, err := os.ReadDir(dir)
 		if err != nil {
-			fmt.Printf("Could not open file: %e", err)
+			//fmt.Printf("Could not open file: %e", err)
 			break
 		}
 		for _, entry := range de {
-			if comm == entry.Name() && !entry.Type().IsDir() {
+			if comm == entry.Name() {
 				cdir := filepath.Join(dir, comm)
 
 				return true, fmt.Sprintf("%s is a valid command", cdir)
