890dd45f1f0862c698fa355cef14e1ae0890416b
nicholasgswan <nicholasgswan@gmail.com>
2026-05-08 14:01:03 -0700

    codecrafters submit [skip ci]

--- internal/path.go
+++ internal/path.go
@@ -20,6 +20,7 @@ func (p PathArray) CheckIfCommandExists(comm string) (bool, string) {
 	for _, dir := range p {
 		cdir := filepath.Join(dir, comm)
 		if fi, err := os.Stat(cdir); err == nil && !fi.IsDir() {
+			fmt.Printf("File perms for %s is %#o\n", cdir, fi.Mode())
 			return true, fmt.Sprintf("%s is %s\n", comm, cdir)
 		}
 
