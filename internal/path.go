9a7cd84fcae5863dff8e97e462cd5196de875dd9
nicholasgswan <nicholasgswan@gmail.com>
2026-05-08 13:56:19 -0700

    codecrafters submit [skip ci]

--- internal/path.go
+++ internal/path.go
@@ -18,20 +18,9 @@ func GetPathArray() PathArray {
 
 func (p PathArray) CheckIfCommandExists(comm string) (bool, string) {
 	for _, dir := range p {
-		de, err := os.ReadDir(dir)
-		if err != nil {
-			//fmt.Printf("Could not open file: %e", err)
-			break
-		}
-		fmt.Printf("Check for %s in dir %s\n", comm, dir)
-
-		for _, entry := range de {
-			fmt.Printf("%s found in dir %s\n", entry.Name(), dir)
-			if comm == entry.Name() {
-				cdir := filepath.Join(dir, comm)
-
-				return true, fmt.Sprintf("%s is a valid command", cdir)
-			}
+		cdir := filepath.Join(dir, comm)
+		if _, err := os.Stat(cdir); err == nil {
+			return true, fmt.Sprintf("%s is a valid command", cdir)
 		}
 
 	}
