f00c64154c085abed1feed9f7a4ee64c00c11836
nicholasgswan <nicholasgswan@gmail.com>
2026-05-08 13:34:11 -0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -11,13 +11,13 @@ var _ = fmt.Print
 
 func main() {
 	// TODO: Uncomment the code below to pass the first stage
-	p := repl.GetPathArray()
-	for _, v := range p {
-		fmt.Println(v)
-	}
+	// p := repl.GetPathArray()
+	// for _, v := range p {
+	// 	fmt.Println(v)
+	// }
 
-	fmt.Println(p.CheckIfCommandExists("badcommand"))
-	fmt.Println(p.CheckIfCommandExists("exercism.exe"))
+	// fmt.Println(p.CheckIfCommandExists("badcommand"))
+	// fmt.Println(p.CheckIfCommandExists("exercism.exe"))
 
 	//fmt.Println(os.Getenv("PATH"))
 	repl.StartRepl()
