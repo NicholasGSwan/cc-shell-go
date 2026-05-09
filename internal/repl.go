c45d7fa16438473fa74096cf77b49de0a131e913
nicholasgswan <nicholasgswan@gmail.com>
2026-05-09 00:44:33 -0700

    codecrafters submit [skip ci]

--- internal/repl.go
+++ internal/repl.go
@@ -80,6 +80,9 @@ func StartRepl() {
 
 		command := strings.TrimSpace(strArr[0])
 		strArr = strArr[1:]
+		for _, v := range strArr {
+			fmt.Println(v)
+		}
 		if strArr[len(strArr)-1] == "" {
 			strArr = strArr[:len(strArr)-1]
 		}
